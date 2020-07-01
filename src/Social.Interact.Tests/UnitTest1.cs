16f568e88697dfd267790f3169b528d7692820ba
Matthew Basson <matthewb@innomatix.co.uk>
2020-07-01 16:08:55 +0100

    Updates to include new feature

---
+++ src/Social.Interact.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Social.Interact.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
